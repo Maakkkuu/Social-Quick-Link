9b89c82314718a0d1308b581fe7a9201275f63d2
Mark Jaily Peña <89086346+Maakkkuu@users.noreply.github.com>
2024-08-25 23:29:31 +0800

    fix: correct the facebook link

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
       <hr></hr>
       <h2>Socials</h2>
       <div className='link-container'>
-        <Button icon="Facebook" text="Facebook" link="https://www.facebook.com/markjaily.pena.9" />
+        <Button icon="Facebook" text="Facebook" link="https://www.facebook.com/makku.jaily" />
         <Button icon="Instagram" text="Instagram" link="https://www.instagram.com/makkukuma/" />
         <Button icon="Github" text="Github" link="https://github.com/Maakkkuu" />
         <Button icon="Linkedin" text="Linkedin" link="https://www.linkedin.com/in/makkukuma/" />
